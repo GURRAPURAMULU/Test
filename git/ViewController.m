a98a8d2105babb39622915887dc0e4d2b26c7509
Ramesh SDSMacMini <ramesh_sdsmacmini@ramesh-mac-mini.local>
2017-07-07 15:48:53 +0530

    Initial Commit

---
+++ git/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  git
+//
+//  Created by MacBook Pro on 07/07/17.
+//  Copyright © 2017 SDS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+//ramesh1
+@end
