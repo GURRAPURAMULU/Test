9bf7295bff524ca83cc0c57c3dc6b3aa775d6fc5
sds macbook <sdsmacbook@ramu.local>
2017-07-07 15:39:38 +0530

    Initial Commit

---
+++ git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  git
+//
+//  Created by MacBook Pro on 07/07/17.
+//  Copyright © 2017 SDS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ git/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  git
+//
+//  Created by MacBook Pro on 07/07/17.
+//  Copyright © 2017 SDS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
